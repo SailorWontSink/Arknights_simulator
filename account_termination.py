bb24d3197d13c9b50c72517f668caabd246d929b
SailorWontSink <xiang62917989@163.com>
2020-07-21 16:42:39 +0800

    Update account_termination.py
    文档注释完成，返回值改善

--- account_termination.py
+++ account_termination.py
@@ -1,7 +1,12 @@
 import json
 import os
 
+#------------------------------------------------------------------------
 
+# acccount_termination(QQ, 素材位置) 注销账户 
+# 如用户存在，删除user_list.txt中该用户数据及'用户数据'文件夹下'[QQ].json'用户
+# 数据文件，返回值为提示信息
+# 如用户不存在，仅返回提示信息
 def account_termination(ID, loc = ''):
     filename = loc + '用户数据\\user.txt'
     try:
@@ -17,6 +22,11 @@ def account_termination(ID, loc = ''):
             json.dump(user_list, file_object)
         filename = loc + '用户数据\\' + str(ID) + '.json'
         os.remove(filename)
-        print('Dr.' + user_name + '已离职，相关档案已销毁且无法恢复')
+        return 'Dr.' + user_name + '已离职，相关档案已销毁且无法恢复'
     else:
-        print('用户不存在')    
+        return '用户不存在'
+
+#------------------------------------------------------------------------
+
+# 后续改进：
+# 在文件删除之前二次提示用户进行确认，以防误操作导致数据丢失
